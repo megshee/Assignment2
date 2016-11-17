098a723085dcbc38b9ac5c946dbe27d2967b6f37
Meghan <megshee@umich.edu>
2016-11-17 17:55:03 -0500

    changes

---
+++ app/src/main/java/com/example/meghan/assignment2/Main2Activity.java
@@ -0,0 +1,51 @@
+package com.example.meghan.assignment2;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class Main2Activity extends Activity implements View.OnClickListener{
+
+    private Button buttonGo;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        buttonGo = (Button) findViewById(R.id.buttonGo);
+        buttonGo.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        //Intent intent = new Intent(this, MainActivity.class);
+        //startActivity(intent);
+
+        new AlertDialog.Builder(this)
+                .setMessage("Are you sure you want to logout?")
+                .setPositiveButton("Yes, Logout", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Intent intent = new Intent(Main2Activity.this, MainActivity.class);
+                        startActivity(intent);
+                    }
+                })
+                .setNegativeButton("No, Stay Here", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(Main2Activity.this, "Staying here", Toast.LENGTH_SHORT).show();
+                    }
+                }).show();
+
+    }
+
+}
+
+
+
--- app/src/main/java/com/example/meghan/assignment2/MainActivity.java
+++ app/src/main/java/com/example/meghan/assignment2/MainActivity.java
@@ -1,13 +1,75 @@
 package com.example.meghan.assignment2;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RatingBar;
+import android.widget.Toast;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements View.OnClickListener {
+
+    //Declare all widgets
+    private Button buttonGo;
+    private EditText editTextLogin;
+    private EditText editTextPassword;
+    private RadioButton radioButtonSettings;
+    private RadioButton radioButtonProfile;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Link Java Objects to Widgets in the UI
+
+        buttonGo = (Button) findViewById(R.id.buttonGo);
+        editTextLogin = (EditText) findViewById(R.id.editTextLogin);
+        editTextPassword = (EditText) findViewById(R.id.editTextPassword);
+        radioButtonSettings = (RadioButton) findViewById(R.id.radioButtonSettings);
+        radioButtonProfile = (RadioButton) findViewById(R.id.radioButtonProfile);
+
+        buttonGo.setOnClickListener(this);
+        //radioButtonSettings.setOnClickListener(this);
+        //radioButtonProfile.setOnClickListener(this);
+
     }
+
+
+    @Override
+    public void onClick(View v) {
+        Intent intent1 = new Intent(this, Main3Activity.class);
+        Intent intent2 = new Intent(this, Main2Activity.class);
+
+        //Event Handler Settings
+        if (v == findViewById(R.id.buttonGo) & editTextLogin.getText().toString().equals("User")
+                & editTextPassword.getText().toString().equals("1234"))
+
+            //Do if true
+            {
+                if (radioButtonProfile.isChecked()) {
+                    startActivity(intent1);
+                } else if (radioButtonSettings.isChecked()){
+                    startActivity(intent2);
+                } else {
+                    Toast.makeText(this, "Select At Least One Radio Button", Toast.LENGTH_SHORT).show();
+                }
+
+
+        } else {
+            //Do if false
+            Toast.makeText(this, "Login Denied", Toast.LENGTH_SHORT).show();
+            editTextLogin.getText().clear();
+            editTextPassword.getText().clear();
+        }
+
+    }
+
+
 }
